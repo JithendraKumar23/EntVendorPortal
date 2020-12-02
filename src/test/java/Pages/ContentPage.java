82f58e4c71853b0eac22545868a5e9ab9a06e9c7
Jeethu <rajeshthakur@gmail.com>
2020-12-02 09:56:25 +0530

    Content Class Created

---
+++ src/test/java/Pages/ContentPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+
+import Constant.BasePage;
+
+public class ContentPage extends BasePage {
+
+	public ContentPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
